adbdf38d07d048ee1105d37ca3c9bb5b188cd05f
Efraim Kent <efraim.kent@student.forsbergsskola.se>
2021-11-09 17:14:46 +0100

    Moves triangle down

--- Program.cs
+++ Program.cs
@@ -20,6 +20,8 @@ namespace SharpEngine {
 
             LoadTriangleIntoBuffer();
 
+            LoadTriangleIntoBuffer();
+
             CreateShaderProgram();
 
             // engine rendering loop
@@ -30,14 +32,15 @@ namespace SharpEngine {
                 glDrawArrays(GL_TRIANGLES, 0, 3);
                 glFlush();
                 //vertices[0] += 0.001f; //BL: x
-                //vertices[1] += 0.001f; //BL: y
+                vertices[1] -= 0.001f; //BL: y
                 //vertices[2] += 0.001f; //BL: z
                 //vertices[3] += 0.001f; //BR: x
-                //vertices[4] += 0.001f; //BR: y
+                vertices[4] -= 0.001f; //BR: y
                 //vertices[5] += 0.001f; //BR: z
                 //vertices[6] += 0.001f; //T: x
-                //vertices[7] += 0.001f; //T: y
+                vertices[7] -= 0.001f; //T: y
                 //vertices[8] += 0.001f; //T: z
+
                 UpdateTriangleBuffer();
             }
         }
