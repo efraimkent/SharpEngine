60eaf21ef04438afb02fad8a8f7125ab72e24c93
Efraim Kent <efraim.kent@student.forsbergsskola.se>
2021-11-09 10:45:54 +0100

    Adds Empty Window

--- Program.cs
+++ Program.cs
@@ -4,22 +4,14 @@ using GLFW;
 namespace SharpEngine {
     class Program {
         static void Main(string[] args) {
+            Glfw.Init();
 
-            var window = new NativeWindow(800, 600, "MyWindowTitle");
+            var window = Glfw.CreateWindow(1024, 756, "SharpEngine", Monitor.None, Window.None);
+            Glfw.MakeContextCurrent(window);
 
-                // Main application loop
-                while (!window.IsClosing) {
-                    // OpenGL rendering
-                    // Implement any timing for flow control, etc (see Glfw.GetTime())
-
-                    // Swap the front/back buffers
-                    window.SwapBuffers();
-
-                    // Poll native operating system events (must be called or OS will think application is hanging)
-                    Glfw.PollEvents();
-                }
-                //"using window..." calls:
-                window.Dispose();
+            while (!Glfw.WindowShouldClose(window)) {
+                Glfw.PollEvents(); // react to changes (position etc)
+            }
         }
     }
 }
