7242da98b91767a20fdabf4bc5c51e62f74003b5
Efraim Kent <efraim.kent@student.forsbergsskola.se>
2021-11-09 17:31:23 +0100

    Scales triangle down

--- Program.cs
+++ Program.cs
@@ -31,17 +31,18 @@ namespace SharpEngine {
                 glClear(GL_COLOR_BUFFER_BIT);
                 glDrawArrays(GL_TRIANGLES, 0, 3);
                 glFlush();
-                //vertices[0] += 0.001f; //BL: x
-                vertices[1] -= 0.001f; //BL: y
-                //vertices[2] += 0.001f; //BL: z
-                //vertices[3] += 0.001f; //BR: x
-                vertices[4] -= 0.001f; //BR: y
-                //vertices[5] += 0.001f; //BR: z
-                //vertices[6] += 0.001f; //T: x
-                vertices[7] -= 0.001f; //T: y
-                //vertices[8] += 0.001f; //T: z
 
-                UpdateTriangleBuffer();
+                    vertices[0] += 0.001f; //BL: x
+                    vertices[1] += 0.001f; //BL: y
+                    //vertices[2] += 0.001f; //BL: z
+                    vertices[3] -= 0.001f; //BR: x
+                    vertices[4] += 0.001f; //BR: y
+                    //vertices[5] += 0.001f; //BR: z
+                    //vertices[6] += 0.001f; //T: x
+                    vertices[7] -= 0.001f; //T: y
+                    //vertices[8] += 0.001f; //T: z
+
+                    UpdateTriangleBuffer();
             }
         }
 
